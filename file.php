1bac072686ef15714afa14116d5f4976c38e866a
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2021-07-02 22:28:25 +0100

    Data types and strings

--- file.php
+++ file.php
@@ -9,11 +9,13 @@
 <body>
 
 <?php
+
 echo "<h1>Codsmiles's site</h1>";
 echo "<hr/>";
 echo "<p>This is my site</p>";
 
 
+// variable
 $characterName = "Tom";
 $characterAge = 50;
 
@@ -27,10 +29,34 @@ echo "He was $characterAge years old<br/>";
 
 // george and 70
 echo "He really likes the name George <br/>";
-echo "But he didnt like being 70 <br/>"
+echo "But he didn't like being 70 <br/>"
 
 
 ?>
-    
+
+
+<?php
+// Data type
+$phrase = "This Is a string(To be or not to be)";
+$age = 30;
+$gpa = 4.5;
+$isMale = true;
+$isFemale = false;
+$sense = null;
+
+// Working with strings in PHP
+$truewords = "Life no balance";
+echo "$truewords<br/>";
+
+// String Functions
+echo strtolower("$truewords<br/>");
+echo strtoupper("$truewords <br/>");
+echo strlen("$truewords");
+echo $truewords[0];
+
+echo str_replace("no", "is", $truewords);
+echo substr ($truewords, 8);
+
+?>
 </body>
 </html>
